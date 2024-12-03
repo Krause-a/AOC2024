02b2a07219bb331d528c6561f3ca937173d721ae
alex krause <alex@populi.co>
2024-12-03 08:04:51 -0800

    Day 1 complete

--- day_01/main.go
+++ day_01/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"aoc_2024/utils"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -14,13 +17,51 @@ func main() {
 }
 
 func part_1(input string) {
-	println("Part 1 START")
-	println(input)
-	println("Part 1 END")
+	list1, list2 := parseLists(input)
+	slices.Sort(list1)
+	slices.Sort(list2)
+
+	var diffSum int
+	for i := range list1 {
+		if list1[i] > list2[i] {
+			diffSum += list1[i] - list2[i]
+		} else {
+			diffSum += list2[i] - list1[i]
+		}
+	}
+
+	println(diffSum)
 }
 
 func part_2(input string) {
-	println("Part 2 START")
-	println(input)
-	println("Part 2 END")
+	numberFrequencies := make(map[int]int)
+
+	list1, list2 := parseLists(input)
+	for _, num := range list2 {
+		numberFrequencies[num]++
+	}
+
+	var similarityScore int
+
+	for _, num := range list1 {
+		similarityScore += num * numberFrequencies[num]
+	}
+
+	println(similarityScore)
+}
+
+func parseLists(input string) ([]int, []int) {
+	var list1 []int
+	var list2 []int
+	for _, line := range strings.Split(input, "\n") {
+		if len(line) == 0 {
+			break
+		}
+		numbers := strings.Split(line, "   ")
+		num1, _ := strconv.Atoi(numbers[0])
+		num2, _ := strconv.Atoi(numbers[1])
+		list1 = append(list1, num1)
+		list2 = append(list2, num2)
+	}
+	return list1, list2
 }
