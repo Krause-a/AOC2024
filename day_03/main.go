86a10dc9dd49a1e3137a0c15d7409688a0bd5f01
alex krause <alex@populi.co>
2024-12-03 09:10:50 -0800

    Day 3 complete

--- day_03/main.go
+++ day_03/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"aoc_2024/utils"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -14,13 +17,36 @@ func main() {
 }
 
 func part_1(input string) {
-	println("Part 1 START")
-	println(input)
-	println("Part 1 END")
+	validPattern := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+	numberPattern := regexp.MustCompile(`\d{1,3}`)
+	validStatements := validPattern.FindAllString(input, -1)
+	var total int
+	for _, statement := range validStatements {
+		numbers := numberPattern.FindAllString(statement, -1)
+		num1, _ := strconv.Atoi(numbers[0])
+		num2, _ := strconv.Atoi(numbers[1])
+		total += num1 * num2
+	}
+
+	println(total)
 }
 
 func part_2(input string) {
-	println("Part 2 START")
-	println(input)
-	println("Part 2 END")
+	input = strings.ReplaceAll(input, "\n"," ")
+	doPattern := regexp.MustCompile(`(^|do\(\)).*?(don't\(\)|\n|$)`)
+	validPattern := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+	numberPattern := regexp.MustCompile(`\d{1,3}`)
+	doSections := doPattern.FindAllString(input, -1)
+	var total int
+	for _, section := range doSections {
+		validStatements := validPattern.FindAllString(section, -1)
+		for _, statement := range validStatements {
+			numbers := numberPattern.FindAllString(statement, -1)
+			num1, _ := strconv.Atoi(numbers[0])
+			num2, _ := strconv.Atoi(numbers[1])
+			total += num1 * num2
+		}
+	}
+
+	println(total)
 }
