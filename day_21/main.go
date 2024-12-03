30caa43bd5fb051d8b039667dd06ebb63082a3db
alex krause <alex@populi.co>
2024-12-02 16:51:31 -0800

    Shell out all days

---
+++ day_21/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"aoc_2024/utils"
+)
+
+func main() {
+	input, part := utils.ParseInput(2)
+	if part == 1 {
+		part_1(input)
+	} else {
+		part_2(input)
+	}
+}
+
+func part_1(input string) {
+	println("Part 1 START")
+	println(input)
+	println("Part 1 END")
+}
+
+func part_2(input string) {
+	println("Part 2 START")
+	println(input)
+	println("Part 2 END")
+}
--- utils/utils.go
+++ utils/utils.go
@@ -6,19 +6,60 @@ import (
 	"strings"
 )
 
-func Test() {
-	fmt.Println("This is a test!")
+func ParseInput(day int) (string, int){
+	input_err_string := "Expected a two arguments. t/r and 1/2 for test/run and part 1/part 2. Example `go run main.go t 2` for run test for part 2"
+	if len(os.Args) != 3 {
+		fmt.Println(input_err_string)
+		os.Exit(1)
+	}
+
+	run_type := strings.Trim(os.Args[1], " \n")
+	if run_type != "t" && run_type != "r" {
+		fmt.Println(input_err_string)
+		os.Exit(1)
+	}
+
+	part := strings.Trim(os.Args[2], " \n")
+	if part != "1" && part != "2" {
+		fmt.Println(input_err_string)
+		os.Exit(1)
+	}
+
+	var part_num int
+	if part == "1" {
+		part_num = 1
+	} else {
+		part_num = 2
+	}
+
+	var input string
+	if run_type == "t" {
+		input = readTest(day, part_num)
+	} else {
+		input = readInput(day)
+	}
+
+	return input, part_num
 }
 
-func ParseInput() {
-	if len(os.Args) != 2 {
-		fmt.Println("Expected a single argument 1 or 2 to represent which part of the day is to be ran")
-		os.Exit(1)
+func readTest(day int, part int) string {
+	return readFile(day, fmt.Sprintf("test_%d", part))
+}
+
+func readInput(day int) string {
+	return readFile(day, "input")
+}
+
+func readFile(day int, filename string) string {
+	file_str, err_1 := os.ReadFile(fmt.Sprintf("day_%d/%s", day, filename))
+
+	if err_1 != nil {
+		file_str_2, err_2 := os.ReadFile(filename)
+		if err_2 != nil {
+			fmt.Println(err_2)
+			os.Exit(3)
+		}
+		file_str = file_str_2
 	}
-	part := strings.Trim(os.Args[1], " \n")
-	if part != "1" && part != "2" {
-		fmt.Println("Expected a single argument 1 or 2 to represent which part of the day is to be ran")
-		os.Exit(1)
-	}
-	fmt.Println(part)
+	return string(file_str)
 }
