c2a4eb0be9fce27f7cf28c8194ca318b716cbcaf
alex krause <alex@populi.co>
2024-12-13 13:07:46 -0800

    Day 8 part 2 complete

--- day_08/main.go
+++ day_08/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"aoc_2024/utils"
+	"aoc_2024/utils/vec"
+	"fmt"
 )
 
 func main() {
@@ -14,13 +16,98 @@ func main() {
 }
 
 func part_1(input string) {
-	println("Part 1 START")
-	println(input)
-	println("Part 1 END")
+	freqs, bounds := parseInput(input)
+	antinodes := make(map[vec.Vec]struct{})
+	for _, vecs := range freqs {
+		lines := make([]vec.Line, 0)
+		for i, v := range vecs {
+			a := v
+			for j := range (len(vecs) - i) {
+				b := vecs[j + i]
+				if a == b {
+					continue
+				}
+				lines = append(lines, vec.MakeLine(a, b))
+			}
+		}
+
+		for _, line := range lines {
+			fmt.Printf("%v, %v => %v\n", line.A, line.B, line.AToB())
+			aToB := line.AToB()
+			bToA := line.BToA()
+			antiPoint := aToB.Add(line.B)
+			if bounds.Contains(antiPoint) {
+				antinodes[antiPoint] = struct{}{}
+			}
+			antiPoint = bToA.Add(line.A)
+			if bounds.Contains(antiPoint) {
+				antinodes[antiPoint] = struct{}{}
+			}
+		}
+	}
+
+	println(len(antinodes))
+}
+
+type Freq rune
+type FreqMap map[Freq][]vec.Vec
+func parseInput(input string) (FreqMap, vec.Box) {
+	antenne := make(FreqMap)
+	bounds := utils.EachRuneWithVec(input, func(v vec.Vec, r rune) {
+		if r == '.' {
+			return
+		}
+		antenne[Freq(r)] = append(antenne[Freq(r)], v)
+	})
+	fmt.Printf("%v\n", bounds)
+	return antenne, vec.MakeBox(vec.Zero, bounds.Sub(vec.One))
 }
 
 func part_2(input string) {
-	println("Part 2 START")
-	println(input)
-	println("Part 2 END")
+	freqs, bounds := parseInput(input)
+	antinodes := make(map[vec.Vec]struct{})
+	for _, vecs := range freqs {
+		lines := make([]vec.Line, 0)
+		for i, v := range vecs {
+			a := v
+			for j := range (len(vecs) - i) {
+				b := vecs[j + i]
+				if a == b {
+					continue
+				}
+				lines = append(lines, vec.MakeLine(a, b))
+			}
+		}
+
+		for _, line := range lines {
+			// fmt.Printf("%v ===> %v\n", line.A, line.B)
+			aToBProgress := line.A
+			aToB := line.AToB()
+			aToBStep := aToB.NormalizeToInt()
+			// fmt.Printf("%v -> %v\n", aToB, aToBStep)
+			// fmt.Printf("%v", aToBProgress)
+			for true {
+				aToBProgress = aToBProgress.Add(aToBStep)
+				if !bounds.Contains(aToBProgress) {
+					break
+				}
+				antinodes[aToBProgress] = struct{}{}
+				// fmt.Printf(" -> %v", aToBProgress)
+			}
+			// fmt.Printf("\n")
+			bToAProgress := line.B
+			bToA := line.BToA()
+			bToAStep := bToA.NormalizeToInt()
+			// fmt.Printf("%v -> %v\n", bToA, bToAStep)
+			for true {
+				bToAProgress = bToAProgress.Add(bToAStep)
+				if !bounds.Contains(bToAProgress) {
+					break
+				}
+				antinodes[bToAProgress] = struct{}{}
+			}
+		}
+	}
+
+	println(len(antinodes))
 }
